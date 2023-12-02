b2a659f9ea9c10961daac1040f9840407e90a32a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-12-01 21:31:42 -0300

    Meme ruim

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string fraseDigitada, fraseMeme;
+Console.Write("Usuário, o que você pensa sobre mim? \n");
+fraseDigitada = Console.ReadLine()!;
+
+fraseMeme =
+    $"\n{fraseDigitada.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(fraseMeme);
